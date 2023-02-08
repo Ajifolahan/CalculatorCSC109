ed9e29aea4a3b6b07663cd3a21b5a3a9c918ea35
Momoreoluwa Ayinde <maayinde@quinnipiac.edu>
2023-02-07 19:09:13 -0500

    Gave user option for addition/subtraction

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
-public class Calculator{
-    public static void main(String[] args){
-        System.out.println("Enter a number");
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Enter 2 numbers");
         Scanner scanner = new Scanner(System.in);
         int number1 = scanner.nextInt();
         int number2 = scanner.nextInt();
-        int result = number1 + number2;
+        System.out.println(
+                "Hey, do you want to perform a subtraction or addition on these numbers.Type in subtraction or addition ");
+        String response = scanner.nextLine();
+        scanner.nextLine();
 
-        System.out.println("Result = " + result);
+        if(response.equalsIgnoreCase("Addition")){
+            int result = number1 + number2;
+            
+        } else {
+            int result = number1 - number2;
+            System.out.println("Result = " + result);
+        }
+        
 
+        
 
     }
 
